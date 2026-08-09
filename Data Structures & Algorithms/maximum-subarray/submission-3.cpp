5d5b2b760f3618e2a19896f068bb071ffb62da3c
p-u-n-i-t <pk2437395@gmail.com>
2026-08-09 23:11:08 +0530

    Add: maximum-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-subarray/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+      vector<int>dp(nums.size());
+      dp[0]=nums[0];
+      for(int i=1;i<nums.size();i++){
+        if(dp[i-1]<0) dp[i]=nums[i];
+        else dp[i]=dp[i-1]+nums[i];
+    
+      }
+     
+
+      return *max_element(dp.begin(),dp.end());
+
+    }
+};
